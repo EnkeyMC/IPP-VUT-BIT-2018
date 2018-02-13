4b07ab00c33b916f9b2980e6cdeeeffadd0ae55e
EnkeyMC <enkeymc@gmail.com>
2018-02-13 12:51:20 +0100

    Fixed test, OSUtils

---
+++ classes/php/OSUtils.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * OSUtils
+ *
+ * Abstracts OS specific commands
+ */
+final class OSUtils {
+    const OS_WIN = 'Windows';
+    const OS_UNIX = 'Unix';
+
+    const CMD_DIFF = 'diff';
+
+    /**
+     * OS Command mapping
+     */
+    const OS_COMMANDS = array(
+        self::OS_WIN => [
+            self::CMD_DIFF => 'FC'
+        ],
+        self::OS_UNIX => [
+            self::CMD_DIFF => 'diff'
+        ]
+    );
+
+    private static $instance = null;
+    private $os;
+
+    /**
+     * Singleton constructor
+     */
+    private function __construct() {
+        $this->os = $this->determineOsType();
+    }
+
+    /**
+     * Determine which OS PHP is running on
+     *
+     * @return OS_WIN or OS_UNIX
+     */
+    private function determineOsType() {
+        // Not very good determination, but it'll do
+        if (strpos(php_uname('s'), self::OS_WIN) !== false) {
+            return self::OS_WIN;
+        } else {
+            return self::OS_UNIX;
+        }
+    }
+
+    private function getOSSpecificCommand($cmd) {
+        return self::OS_COMMANDS[$this->os][$cmd];
+    }
+
+    public static function getInstance() {
+        if (self::$instance === NULL)
+            self::$instance = new OSUtils();
+        return self::$instance;
+    }
+
+    public static function checkFileDifference($filename1, $filename2) {
+        $output = array();
+        $rc = 0;
+
+        exec(self::buildCommand(self::CMD_DIFF, [$filename1, $filename2]), $output, $rc);
+
+        return [$output, $rc];
+    }
+
+    private static function buildCommand($cmd, array $args) {
+        $cmd = self::getInstance()->getOSSpecificCommand($cmd);
+        foreach ($args as $arg) {
+            $cmd .= ' ' . $arg;
+        }
+
+        return $cmd;
+    }
+}
--- unittests/php/XMLOutputTest.php
+++ unittests/php/XMLOutputTest.php
@@ -47,14 +47,20 @@ final class XMLOutputTest extends TestCase
 
     public function testStartInstruction() {
         $this->XMLWriterMock->method('startElement')
-                            ->withConsecutive([XMLOutput::EL_PROGRAM, XMLOutput::EL_INSTRUCTION);
+                            ->withConsecutive(
+                                [XMLOutput::EL_PROGRAM],
+                                [XMLOutput::EL_INSTRUCTION]);
         $this->XMLWriterMock->method('startAttribute')
-                            ->withConsecutive([XMLOutput::ATTR_LANGUAGE, XMLOutput::ATTR_ORDER, XMLOutput::ATTR_OPCODE]);
-        $this->XMLWriterMock->expects($this->ex())->method('text')
-                            ->with(1);
-        $this->XMLWriterMock->expects($this->once())->method('text')
-                            ->with(self::TEST_OPCODE);
-        $this->XMLWriterMock->expects($this->exactly(2))->method('endAttribute');
+                            ->withConsecutive(
+                                [XMLOutput::ATTR_LANGUAGE],
+                                [XMLOutput::ATTR_ORDER],
+                                [XMLOutput::ATTR_OPCODE]);
+        $this->XMLWriterMock->method('text')
+                            ->withConsecutive(
+                                [XMLOutput::LANGUAGE],
+                                [1],
+                                [self::TEST_OPCODE]);
+        $this->XMLWriterMock->expects($this->exactly(3))->method('endAttribute');
 
         $xo = new XMLOutput($this->XMLWriterMock);
         $xo->startInstruction(self::TEST_OPCODE);
@@ -68,13 +74,43 @@ final class XMLOutputTest extends TestCase
     }
 
     public function testAddArgument() {
-        $this->XMLWriterMock->expects($this->once())->method('startElement')
-            ->with(XMLOutput::EL_ARG . '1');
-        $this->XMLWriterMock->expects($this->once())->method('text')
-            ->with('test_text');
+        $this->XMLWriterMock->method('startElement')
+                            ->withConsecutive(
+                                [XMLOutput::EL_PROGRAM],
+                                [XMLOutput::EL_ARG . '1']);
+        $this->XMLWriterMock->method('text')
+                            ->withConsecutive(
+                                [XMLOutput::LANGUAGE],
+                                ['label'],
+                                ['test_text']);
         $this->XMLWriterMock->expects($this->once())->method('endElement');
 
         $xo = new XMLOutput($this->XMLWriterMock);
         $xo->addArgument(1, 'label', 'test_text');
+    }
+
+    public function testXMLOutput() {
+        $expectedXML = '<?xml version="1.0" encoding="UTF-8" ?>
+            <program language="IPPcode18">
+                <instruction order="1" opcode="DEFVAR">
+                    <arg1 type="var">GF@var</arg1>
+                </instruction>
+                <instruction order="2" opcode="MOVE">
+                    <arg1 type="var">GF@var</arg1>
+                    <arg2 type="var">GF@var</arg2>
+                </instruction>
+             </program>';
+
+        $xo = new XMLOutput();
+        $xo->startInstruction('DEFVAR');
+        $xo->addArgument(1, 'var', 'GF@var');
+        $xo->endInstruction();
+        $xo->startInstruction('MOVE');
+        $xo->addArgument(1, 'var', 'GF@var');
+        $xo->addArgument(2, 'var', 'GF@var');
+        $xo->endInstruction();
+        $xo->endOutput();
+
+        $this->assertXmlStringEqualsXmlString($expectedXML, $xo->getOutput());
     }
 }
